21c9cb711fe32b03acc4ee92fef35ec370182bcf
Fikir <fikirmuluken@gmail.com>
2021-07-12 15:43:00 +0300

    make the ui and finish the App

---
+++ flutter_demo/lib/features/number_trivia/presentation/widgets/trivia_controls.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_demo/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _TriviaControlsState createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  final controller = TextEditingController();
+  String inputStr;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //textfield
+        TextField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(), hintText: 'Input a number'),
+          onChanged: (value) {
+            inputStr = value;
+          },
+          onSubmitted: (_) {
+            dispatchConcrete();
+          },
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            Expanded(
+                child: ElevatedButton(
+              child: Text('Search'),
+              onPressed: dispatchConcrete,
+            )),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+                child: ElevatedButton(
+              child: Text('Get random trivia'),
+              onPressed: dispatchRandom,
+            )),
+          ],
+        )
+      ],
+    );
+  }
+
+  void dispatchConcrete() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .dispatch(GetTriviaForConcreteNumber(inputStr));
+  }
+
+  void dispatchRandom() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .dispatch(GetTriviaForRandomNumber());
+  }
+}
